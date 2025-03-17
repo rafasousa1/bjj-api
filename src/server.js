646ffe661e82f353209b3506a0aee54cc5b26805
rafasousa <140452345+rafasousa1@users.noreply.github.com>
2025-03-17 19:29:27 -0300

    veryfing the regex

--- src/server.js
+++ src/server.js
@@ -3,16 +3,23 @@ import { json } from './middlewares/json.js'
 import { routes } from './routes.js'
 
 const server = http.createServer(async (req, res) => {
-    const { method, url } = req
     await json(req, res)
 
+    const { method, url } = req
+
     const route = routes.find(route => {
-        return route.method === method && route.url === url
+        return route.method === method && route.url.test(url)
     })
 
     if (route) {
+        const routeParams = req.url.match(route.url)
+
+        req.params = { ...routeParams.groups }
+
         return route.handler(req, res)
     }
+
+    return res.writeHead(404).end()
 })
 
 server.listen(3000)
